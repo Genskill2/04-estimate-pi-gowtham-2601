2406403324add0dda5918b3348bf8b16fa100de5
gowtham-2601 <85103053+gowtham-2601@users.noreply.github.com>
2021-06-04 11:10:18 +0530

    Update wallis.c

--- wallis.c
+++ wallis.c
@@ -4,7 +4,16 @@
 #include <math.h>
 
 float wallis_pi(int);
-
+float wallis_pi(int n)
+{
+  float k=1.0;
+  int i;
+  for(i=1;i<n;i++)
+  {
+    k*=(float)(4.*i*i)/(4.*i*i-1);
+  }
+  return (k*2);
+}
 int main(void) {
   float pi;
   for (int i=0; i<5; i++) {
